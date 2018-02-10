e251589b484532181349bb6c2b4726953103cb8b
Adrian Bavister <adrian@bavanco.co.uk>
2018-02-10 10:36:28 -0600

    Commented out api closure

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,8 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Note: Can't cache routes when using closues
+//
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
